c186ed1d6e8c65aaa2801b61c0694f917f0fd7c4
Ivan <iponomarev@mail.ru>
2026-01-15 18:32:48 +0000

    java example

--- src/main/java/org/example/WordCounter.java
+++ src/main/java/org/example/WordCounter.java
@@ -4,20 +4,21 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+// tag::static_imports[]
 import static java.util.Arrays.stream;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.counting;
+// end::static_imports[]
 
 public class WordCounter {
     public static Map<String, Long> countWords(Stream<String> lines) {
         return lines.
-        // tag::pipeline[]
-        filter(Objects::nonNull)
-          .flatMap(line -> stream(line.split("[^A-Za-z0-9']+")))
-          .filter(word -> !word.isEmpty())
-          .map(String::toLowerCase)
-          .collect(Collectors.groupingBy(identity(), counting()));
-        // end::pipeline[]
+                // tag::pipeline[] @formatter:off
+filter(Objects::nonNull)
+  .flatMap(line -> stream(line.split("[^A-Za-z0-9']+")))
+  .filter(word -> !word.isEmpty())
+  .map(String::toLowerCase)
+  .collect(Collectors.groupingBy(identity(), counting()));
+        // end::pipeline[] @formatter:on
     }
 }
